4974f37a0f9102e1c6d360a278be49cce2a4f6cf
Erik <46608127+Erik-For@users.noreply.github.com>
2024-03-19 11:12:36 +0000

    Fix navitem selected and default color

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -10,7 +10,7 @@ const NavItem = ({ text, Icon, src } : {text: string, Icon: IconType | null, src
     <div onClick={(e) => {
         e.preventDefault();
         navigate(src);
-    }} style={{ color: selected? "#9CA38F" : "#FFFFFF"}} className="active:scale-[.95] transition-transform md:flex-row select-none cursor-pointer flex flex-col flex-start justify-center items-center grow md:grow-0 w-11/12 md:m-1 h-full md:h-10 md:border-2 border-transparent bg-gray-850 md:bg-gray-900 md:rounded-md">
+    }} style={{ color: selected? "#FFFFFF" : "#9CA38F"}} className="active:scale-[.95] transition-transform md:flex-row select-none cursor-pointer flex flex-col flex-start justify-center items-center grow md:grow-0 w-11/12 md:m-1 h-full md:h-10 md:border-2 border-transparent bg-gray-850 md:bg-gray-900 md:rounded-md">
         {Icon ? <Icon className="text-2xl md:text-3xl"/> : ""}
         <p className="font-medium md:pl-2 text-xs md:text-sm">{text}</p>
     </div>
